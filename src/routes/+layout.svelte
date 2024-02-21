ec21be4ce91094fb7ff516b1c886e09de8b86b6a
fedevcoding <fedevcoding@gmail.com>
2024-02-21 14:38:24 +0100

    style header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <section>
- <header>Header</header>
+ <header
+  class="h-20 border-b-gray-500 border-b-2 flex items-center justify-between px-12"
+ >
+  <h1 class="text-2xl font-semibold">Css box shadows</h1>
+  <button class="bg-blue-600 py-2 px-6 rounded-md">Get code</button>
+ </header>
 
  <slot />
 </section>
