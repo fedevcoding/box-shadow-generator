0e340eabc04109e4941f7112d9cd6efce9da70f9
fedevcoding <fedevcoding@gmail.com>
2024-02-22 10:28:20 +0100

    🗒️ separate header in component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script>
  import "./app.css";
+ import Header from "./lib/Header.svelte";
 </script>
 
 <section>
- <header
-  class="h-20 border-b-gray-500 border-b-2 flex items-center justify-between px-12"
- >
-  <h1 class="text-2xl font-semibold">Css box shadows</h1>
-  <button class="bg-blue-600 py-2 px-6 rounded-md">Get code</button>
- </header>
+ <Header />
 </section>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,6 @@
+<header
+ class="h-20 border-b-gray-500 border-b-2 flex items-center justify-between px-12"
+>
+ <h1 class="text-2xl font-semibold">Css box shadows</h1>
+ <button class="bg-blue-600 py-2 px-6 rounded-md">Get code</button>
+</header>
