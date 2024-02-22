7be2022b278a1b6669de1855e50b9b4c88ad2ecb
fedevcoding <fedevcoding@gmail.com>
2024-02-22 10:30:56 +0100

    🖼️ add logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+ import img from "../assets/logo.svg";
+</script>
+
 <header
  class="h-20 border-b-gray-500 border-b-2 flex items-center justify-between px-12"
 >
- <h1 class="text-2xl font-semibold">Css box shadows</h1>
+ <div class="flex items-center gap-6">
+  <img src={img} alt="log" class="w-10" />
+  <h1 class="text-2xl font-semibold">Css box shadow</h1>
+ </div>
  <button class="bg-blue-600 py-2 px-6 rounded-md">Get code</button>
 </header>
