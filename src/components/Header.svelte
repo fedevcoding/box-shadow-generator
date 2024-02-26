08401245099b5b19b4dd88645aa03411bda34ebe
fedevcoding <fedevcoding@gmail.com>
2024-02-26 07:33:45 +0100

    style header copy

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,9 +7,18 @@
  export let shadows: Shadow[];
 
  $: code = getBoxShadow(shadows);
+ let status: "Copy" | "Copied!" = "Copy";
+
+ function copy() {
+  status = "Copied!";
+  navigator.clipboard.writeText(`box-shadow: ${code};`);
+  setTimeout(() => {
+   status = "Copy";
+  }, 1000);
+ }
 
  const handleClick = () => {
-  (document.getElementById("my_modal_1") as any).showModal();
+  (document.getElementById("code-modal") as any).showModal();
  };
 </script>
 
@@ -27,23 +36,41 @@
   </a>
 
   <button class="d-btn d-btn-primary" on:click={handleClick}>Get code</button>
-  <dialog id="my_modal_1" class="d-modal">
-   <div class="d-modal-box">
+  <dialog id="code-modal" class="d-modal">
+   <div class="d-modal-box w-[30vw] max-w-3xl">
     <h3 class="font-bold text-lg">Generated styles</h3>
     <p class="py-4">
      This is the CSS needed to generate the box shadows you see in the app.
     </p>
 
-    <div class="mockup-code overflow-x-scroll">
-     <pre><code>box-shadow: {code};</code></pre>
+    <div class="mockup-code flex gap-4 items-center">
+     <pre class="bg-gray-950 py-5 px-2 rounded-md overflow-x-scroll"><code
+       >box-shadow: {code};</code
+      ></pre>
+     <button class="d-btn d-btn-secondary" on:click={copy}>{status}</button>
     </div>
 
     <div class="d-modal-action">
      <form method="dialog">
-      <button class="d-btn">Close</button>
+      <button class="d-btn d-btn-error">Close</button>
      </form>
     </div>
    </div>
   </dialog>
  </div>
 </header>
+
+<style>
+ pre::-webkit-scrollbar-track {
+  background-color: #f5f5f5;
+ }
+
+ pre::-webkit-scrollbar {
+  height: 2px;
+  background-color: #f5f5f5;
+ }
+
+ pre::-webkit-scrollbar-thumb {
+  background-color: #575757;
+ }
+</style>
